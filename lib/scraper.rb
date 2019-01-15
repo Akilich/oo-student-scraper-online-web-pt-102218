d3f92de19c7313521dfdf75f17aeac5e62f4b299
Akili Humphrey <akilihumphrey@gmail.com>
2019-01-15 05:34:01 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,17 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = open(index_url).read
+    html = Nokogiri::HTML(doc)
+
+    html.css('div.student-card').map do |student|
+      {
+        name: student.css('a div.card-text-container h4').text,
+        location: student.css('a div.card-text-container p').text,
+        profile_url: student.css('a').attribute('href').value
+      }
+    end
+  end
   end
 
   def self.scrape_profile_page(profile_url)
