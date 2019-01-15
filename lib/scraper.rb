7fa673c5a7a62b08cc1a0579a03e776bb416a863
Akili Humphrey <akilihumphrey@gmail.com>
2019-01-15 05:34:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,7 +18,28 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = open(profile_url).read
+    html = Nokogiri::HTML(doc)
+    profile = {}
+
+
+    profile[:profile_quote] = html.css("div.vitals-text-container div.profile-quote").text
+    profile[:bio] = html.css("div.bio-content.content-holder div.description-holder p").text
+
+    html.css("div.social-icon-container a").each do |social|
+      if social.attribute("href").text.include?("twitter")
+        profile[:twitter] = social.attribute("href").text
+      elsif social.attribute("href").text.include?("linkedin")
+        profile[:linkedin] = social.attribute("href").text
+      elsif social.attribute("href").text.include?("github")
+        profile[:github] = social.attribute("href").text
+      else
+        profile[:blog] = social.attribute("href").text
+      end
+    end
+    profile
+  end
+end  
   end
 
 end
